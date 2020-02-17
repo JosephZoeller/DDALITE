0675c4ae8d8bc0c5910b544c41a3905aed2c5096
JesseyBlan <jesseybland@gmail.com>
2020-02-17 09:58:08 -0600

    Merged new script pulling sudo out

--- scripts/master.deploy.sh
+++ scripts/master.deploy.sh
@@ -1,4 +1,4 @@
 
 export masterip=$(terraform output master_ip)
-xfce4-terminal -e " ssh -i ../terraform/secrets/private.pem ubuntu@$masterip "&
+xfce4-terminal -e " ssh -i ../terraform/secrets/private.pem ubuntu@$masterip " &
 
--- scripts/prep_master_node.sh
+++ scripts/prep_master_node.sh
@@ -5,11 +5,9 @@
 echo Setting up Kubernetes Node...
 
 # Update instance repos and install standard software.
-sudo apt-get update
+apt-get update
 
-sudo su -
-
-apt-get install \
+apt-get install -y\
     apt-transport-https \
     ca-certificates \
     curl \
@@ -17,7 +15,7 @@ apt-get install \
     software-properties-common
 
 # Add key for docker-ubuntu repo
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  apt-key add -
 
 # Add repo to end of /etc/apt/source.list
 add-apt-repository \
@@ -27,13 +25,13 @@ add-apt-repository \
 
 # Update new docker repo and install modern docker tools
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Update repo list with kubernetes tools and install the 3 universal tools
 # all nodes are expected to have.
-apt-get update && sudo apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update &&  apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg |  apt-key add -
+cat <<EOF |  tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
@@ -43,17 +41,17 @@ apt-get install -y kubelet kubeadm kubectl
 # The next portions are specific to the master node.
 
 # Kubeadm prepares master node
-sudu su -
+
 
 kubeadm init
 
-exit
+
 
 # Make a directory with kubernetes configuration inside HOME.
 # Give file super user permissions?
 mkdir -p $HOME/.kube
-  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+   cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+   chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Check that what nodes are currently in cluster.
 # Then check what pods are available across all namespaces.
@@ -61,15 +59,24 @@ kubectl get node
 kubectl get pods --all-namespaces
 
 # I have no idea what is going on here.
-sudo su -
+
 sysctl net.bridge.bridge-nf-call-iptables=1
 
 # Start clouds.weave.works with kubernetes version passed in
 # and also set as an env variable.
-exit 
+
 export kubever=$(kubectl version | base64 | tr -d '\n')
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-exit
+
+var1=$(sudo kubeadm token create --print-join-command)
+
+arrvar1=(${var10// / })
+
+masteripp=$(echo ${arrvar10[2]})
+
+token=$(echo ${arrvar10[4]})
+
+discovery_token=$(echo ${arrvar10[6]})
 
 # Check for all nodes once more.
 kubectl get nodes
