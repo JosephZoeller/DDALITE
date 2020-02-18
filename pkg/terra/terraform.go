ce64251d259f2f15067d5b93900c8cf73572789f
ken343 <kmcdole21@gmail.com>
2020-02-18 17:27:37 -0600

    Adding User Defined Terraform Package
    This package will handle all the provisioning.

---
+++ pkg/terra/terraform.go
@@ -0,0 +1,10 @@
+// Package terra will hand function related to manipulating terraform
+// configuration files.
+package terra
+
+// Provision will create all the slave nodes based on what client
+// send in request.
+// May change to pass in master token.
+func Provision(instanceCount string) {
+
+}
