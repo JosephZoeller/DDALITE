1edcfc27c2fd5612ead4e897640de37d22bcc515
ken343 <kmcdole21@gmail.com>
2020-02-18 20:43:42 -0600

    Added Database Length; SDNC submits POST Request
    Add Kubernetes Next

--- cmd/sdnc/init.go
+++ cmd/sdnc/init.go
@@ -6,7 +6,7 @@ const (
 )
 
 var (
-	dictionaryLength string = 10 //!! WARNING THIS IS ONLY TEMPORARY PLEASE ADD INIT LOGIC TO GET ACTUAL LENGTH FROM DB
+	dictionaryLength string = 466550 //!! WARNING THIS IS ONLY TEMPORARY PLEASE ADD INIT LOGIC TO GET ACTUAL LENGTH FROM DB
 	workerAddrs      []string
 )
 
--- cmd/sdnc/workercomms.go
+++ cmd/sdnc/workercomms.go
@@ -10,10 +10,16 @@ import (
 
 // Send hash to each worker's ip address.
 func sendToWorkers(hash string, workerAddrs []string) {
+	workerCount := len(workerAddrs)                // How many slave ips have we registered?
+	var pages int = dictionaryLength / workerCount // How big will the assigned tasks will be
+
+	// Iterate over worker addresses and submit a hash, startindex, and length of entries.
 	for i := 0; i < len(workerAddrs); i++ {
+		startIndex := i * pages
 		go func(index int) {
-			resp, er := tryGet(workerAddrs[index], hash, 10)
+			resp, er := tryGet(workerAddrs[index], hash, startIndex, pages, 15)
 			if er == nil {
+				// Use proper logging in the future.
 				resp.Write(os.Stdout)
 			} else {
 				log.Fatal("Timeout: failed to connect - ", er)
@@ -23,16 +29,20 @@ func sendToWorkers(hash string, workerAddrs []string) {
 }
 
 // tryGet attempts to send the hash string to the address every second, for t seconds. If no connection is made in that time, returns an error.
-func tryGet(addr, hash string, t int) (*http.Response, error) {
+func tryGet(addr, hash string, index int, length int, t int) (*http.Response, error) {
 	var er error
 	var resp *http.Response
 	var colliderPort string = "8080"
 
 	for i := 0; i < t; i++ {
-		// Submit Get request to colliders. Post request will not be used because GET query and
+		// Submit request to colliders. Post request will not be used because GET query and
 		// POST Content-Type: application/x-www-form-urlencoded get parsed exactly the same by
 		// myURL.PasreForm()
-		resp, er = http.Get(fmt.Sprintf("%s:%s/?hash=%s&start=%s&end=%s", addr, colliderPort, hash))
+		colliderURL := fmt.Sprintf("http://%s:%s/", addr, colliderPort)
+		contentType := "application/x-www-form-urlencoded"
+		content := fmt.Sprintf("hash=%s&index=%s&length=%s", hash, index, length)
+		resp, er = http.Post(colliderURL, contentType, content)
+		defer resp.Close()
 		// Normal operation
 		if er == nil {
 			return resp, er
