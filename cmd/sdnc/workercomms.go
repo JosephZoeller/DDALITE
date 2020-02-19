f63862a44beeaf6052b7c331400d84ebea27d628
ken343 <kmcdole21@gmail.com>
2020-02-19 01:03:03 -0600

    readability and comments

--- cmd/sdnc/workercomms.go
+++ cmd/sdnc/workercomms.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-// Send hash to each worker's ip address.
+// Send hash to each worker's overlay ip address.
 func sendToWorkers(hash string, workerAddrs []string) *http.Response {
 	workerCount := len(workerAddrs)                // How many slave ips have we registered?
 	var pages int = dictionaryLength / workerCount // How big will the assigned tasks will be
@@ -15,6 +15,8 @@ func sendToWorkers(hash string, workerAddrs []string) *http.Response {
 	// Iterate over worker addresses and submit a hash, startindex, and length of entries.
 	for i := 0; i < len(workerAddrs); i++ {
 		startIndex := i * pages
+
+		// This go routine submits values to the PODS not the ec2s.
 		go func(index int) {
 			resp, er := tryGet(workerAddrs[index], hash, startIndex, pages, 15)
 			if er == nil {
@@ -33,23 +35,30 @@ func tryGet(addr, hash string, index int, length int, t int) (*http.Response, er
 	var colliderPort string = "8080"
 
 	for i := 0; i < t; i++ {
-		// Submit request to colliders. Post request will not be used because GET query and
+
+		// Submit request to colliders. Post request will be used because GET query and
 		// POST Content-Type: application/x-www-form-urlencoded get parsed exactly the same by
 		// myURL.PasreForm()
 		colliderURL := fmt.Sprintf("http://%s:%s/", addr, colliderPort)
 		contentType := "application/x-www-form-urlencoded"
 		content := fmt.Sprintf("hash=%s&index=%s&length=%s", hash, index, length)
+
 		resp, er = http.Post(colliderURL, contentType, content)
 		defer resp.Close()
+
 		// Normal operation
 		if er == nil {
 			return resp, er
 		}
+
 		// Print error to standard console and wait to try again.
 		fmt.Println(er)
 		time.Sleep(time.Second)
+
 	}
+
 	// We did not connect to collider in time. Don't return a resp back to sendToWorkers.
-	// Return error from GET request.
+	// Return error from POST request.
 	return nil, er
+
 }
