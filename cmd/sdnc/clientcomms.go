64039d749b7f74127eb8544e09835488e2693024
ken343 <kmcdole21@gmail.com>
2020-02-19 01:41:04 -0600

    SDNC Now Returns Response from Workers
    Next Tear Down Kubernetes and Terraform with new functions.

--- cmd/sdnc/clientcomms.go
+++ cmd/sdnc/clientcomms.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/200106-uta-go/JKJP2/pkg/cityhashutil"
 	"github.com/200106-uta-go/JKJP2/pkg/kubeutil"
 	"github.com/200106-uta-go/JKJP2/pkg/terra"
 )
@@ -16,7 +17,7 @@ func listenForClient() {
 		// Parse Form sent from reverse proxy. Set variables equal and check they aren't empty.
 		err := req.ParseForm()
 		if err != nil {
-			log.Panic("Error Parsing Client Request Form")
+			http.Error(rw, "Error Parsing client request form.", http.StatusInternalServerError)
 		}
 
 		// Double check that variables actually have a value. Send a bad request
@@ -25,27 +26,44 @@ func listenForClient() {
 		instanceCount := req.FormValue("instances")
 
 		if hash == "" || instanceCount == "" {
-			rw.WriteHeader(http.StatusBadRequest)
-			errMsg := fmt.Sprintf("Error hash=%s and instances=%s were not set.", hash, instanceCount)
-			rw.Write([]byte(errMsg))
-			break
+			http.Error(rw, fmt.Sprintf("Error hash=%s and instances=%s were not set.", hash, instanceCount), http.StatusBadRequest)
+			return
 		}
 
 		// Initiate Terraform script to create EC2 instances.
+		// Use the ips here to log the EC2 underlay ips for safekeeping.
 		ips := terra.Provision(instanceCount)
+		fmt.Printf("ECS have been created @ %v", ips)
 
-		// Launch deployment yaml in /kubernetes/deployment.yaml
+		// Launch deployment yaml in /kubernetes/deployment.yaml and return the pod private overlay ips.
 		overIps := kubeutil.SetUp(instanceCount)
 
+		// Send hash to each pod at each overlay ip.
 		resp := sendToWorkers(hash, overIps)
+		resp.Body.Close()
 
-		var myCol cityhashutil.Collision
+		// Store hash and collision in struct
+		var myCollision cityhashutil.HashCollision
 
-		err := json.NewDecoder(&myCol).Decode()
+		err := json.NewDecoder(resp.Body).Decode(&myCollision)
 		if err != nil {
-			log.Panicf("Error decoding json: Error == %v", err)
+			http.Error(rw, fmt.Printf("Error decoding json: Error == %v", err), http.StatusInternalServerError)
+			return
 		}
 
+		// Log result to stdoutput. May want to route logs to different location later.
+		log.Printf("Worker Returned Collision: %v\n", myCollision)
+
+		// Wrap up myCollision into json because you do not want to read response body multiple times.
+		js, err := json.Marshal(myCollision)
+		if err != nil {
+			http.Error(rw, fmt.Printf("Error marshaling json: Error == %v", err), http.StatusInternalServerError)
+			return
+		}
+
+		// Just pass on body from worker back to reverse proxy after marshaling.
+		rw.Header().Set("Content-Type", "application/json")
+		rw.Write(js)
 	})
 
 	http.ListenAndServe(":8080", nil)
