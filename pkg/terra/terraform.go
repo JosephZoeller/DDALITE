21db0f0cbab320f11e15acf29c4363c2a804f398
JesseyBland <jesseybland@gmail.com>
2020-02-19 04:24:53 -0600

    Fixed terraform.go, and added files to ec2.tf master.

--- pkg/terra/terraform.go
+++ pkg/terra/terraform.go
@@ -3,6 +3,8 @@
 package terra
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
 )
 
@@ -10,20 +12,46 @@ import (
 // send in request. This function returns the ips of all the created slave nodes.
 // May change to pass in master token.
 func Provision(instanceCount string) []string {
-	if instanceCount >5 {
-	
+	// IF count=6 deploy 6 ec2s ELSE deploy 3 ec2s.
+	if instanceCount == "6" {
 
-		exec.Command("echo","$(jq '.user_count = \"6\"' var.json)" ">" "var.json")
+		// cmd := exec.Command("bash", "-c", "echo hello > /tmp/aaa")
 
+		cmd := exec.Command("bash", "-c", "echo $(jq '.user_count = \"6\"' var.json) > var.json")
+		cmd.Dir = "/home/ubuntu/terradir"
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+	} else {
+
+		// cmd := exec.Command("bash", "-c", "echo hello > /tmp/aaa")
+
+		cmd := exec.Command("bash", "-c", "echo $(jq '.user_count = \"3\"' var.json) > var.json")
+		cmd.Dir = "/home/ubuntu/terradir"
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
 	}
-	
-	exec.Command("cd", "/home/ubunut/terradir")
-	exec.Command("terraform", "init")
-	exec.Command("terraform", "apply")
+	cmd = exec.Command("/bin/sh", "-c", "sudo terraform init")
+	cmd.Dir = "/home/ubuntu/terradir"
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+	cmd = exec.Command("/bin/sh", "-c", "sudo terraform apply -auto-approve")
+	cmd.Dir = "/home/ubuntu/terradir"
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 
-	exec.Command("export", "instance_ips=$(terraform output instance_ips)")
+	// cmd export instance_ips=$(terraform output instance_ips)
+	cmd = exec.Command("/bin/sh", "-c", "export instance_ips=$(terraform output instance_ips)")
+	cmd.Dir = "/home/ubuntu/terradir"
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+	// cmd print instance_ips
+	fmt.Print(os.Getenv("instance_ips"))
 
 	return os.Getenv("instance_ips")
-
 
 }
