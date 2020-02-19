09327cccc8400df94a9375a28c0f786e660e4f01
JesseyBland <jesseybland@gmail.com>
2020-02-19 10:59:05 -0600

    Fixed terraform.go

--- pkg/terra/terraform.go
+++ pkg/terra/terraform.go
@@ -11,27 +11,13 @@ import (
 // Provision will create all the slave nodes based on what client
 // send in request. This function returns the ips of all the created slave nodes.
 // May change to pass in master token.
-func Provision(instanceCount string) []string {
-	// IF count=6 deploy 6 ec2s ELSE deploy 3 ec2s.
-	if instanceCount == "6" {
-
-		// cmd := exec.Command("bash", "-c", "echo hello > /tmp/aaa")
-
-		cmd := exec.Command("bash", "-c", "echo $(jq '.user_count = \"6\"' var.json) > var.json")
-		cmd.Dir = "/home/ubuntu/terradir"
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Run()
-	} else {
-
-		// cmd := exec.Command("bash", "-c", "echo hello > /tmp/aaa")
-
-		cmd := exec.Command("bash", "-c", "echo $(jq '.user_count = \"3\"' var.json) > var.json")
-		cmd.Dir = "/home/ubuntu/terradir"
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Run()
-	}
+func Provision(instanceCount string) {
+	arg := fmt.Sprintf("echo $(jq '.user_count = %s' var.json) > var.json", instanceCount)
+	cmd := exec.Command("bash", "-c", arg)
+	cmd.Dir = "/home/ubuntu/terradir"
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 	cmd = exec.Command("/bin/sh", "-c", "sudo terraform init")
 	cmd.Dir = "/home/ubuntu/terradir"
 	cmd.Stdout = os.Stdout
@@ -42,16 +28,4 @@ func Provision(instanceCount string) []string {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
-
-	// cmd export instance_ips=$(terraform output instance_ips)
-	cmd = exec.Command("/bin/sh", "-c", "export instance_ips=$(terraform output instance_ips)")
-	cmd.Dir = "/home/ubuntu/terradir"
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-	// cmd print instance_ips
-	fmt.Print(os.Getenv("instance_ips"))
-
-	return os.Getenv("instance_ips")
-
 }
