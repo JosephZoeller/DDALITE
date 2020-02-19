77e115a665bca628e83a07c02b725939eca23e2a
JosephZoeller <joe.h.zoeller@gmail.com>
2020-02-19 12:34:31 -0600

    database connection test and hardcode logic

--- cmd/collider/collider_test.go
+++ cmd/collider/collider_test.go
@@ -4,14 +4,16 @@ package main
 import (
 	"fmt"
 	"testing"
+
+	"github.com/200106-uta-go/JKJP2/pkg/flyerdbutil"
 )
 
-func Test(t *testing.T) {
-	found := findCollisionFile("4132412743", 0, 450000)
+func Test(t *testing.T) { // TENSION_NECK == 60db807ad6c6 (106495869638342) // apple == b10f405db64b (194679062509131) Apple == 4e1ec7e1511b (85894109417755)
+	found := findCollisionFile("194679062509131", 0, 450000)
 	if found != "" {
 		fmt.Println(found)
 	} else {
-		t.Error("Known dictionary hash not found: 41320793412743")
+		t.Error("Known dictionary hash not found: 194679062509131")
 	}
 }
 
@@ -19,5 +21,38 @@ func Benchmark(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		findCollisionFile("1", 0, i)
 	}
+
+}
+
+func TestDB(t *testing.T) { // Apple == 4e1ec7e1511b (85894109417755)
+	var er error
+
+	flyerDB, er = flyerdbutil.ConnectHard()
+	if er != nil {
+		t.Error("Connection Failed - ", er)
+	}
+
+	found := findDBCollision("85894109417755", 0, 450000)
+	if found != "" {
+		fmt.Println(found)
+	} else {
+		t.Error("Known dictionary hash not found: 85894109417755")
+	}
+}
+
+func TestGetWord(t *testing.T) {
+		var word string
+		var er error
+
+		flyerDB, er = flyerdbutil.ConnectHard()
+		if er != nil {
+			t.Error("Connection Failed - ", er)
+		}
+
+		row := flyerDB.QueryRow("SELECT word FROM dic WHERE idx = $1", 0)
+		er = row.Scan(&word)
+		if er != nil {
+			t.Error("Query Failed - ", er)
+		}
 
 }
--- cmd/collider/dictionarydbcompare.go
+++ cmd/collider/dictionarydbcompare.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 
-	"github.com/200106-uta-go/JKJP2/pkg/cityhashutil"
 	"github.com/200106-uta-go/JKJP2/pkg/flyerdbutil"
-	_ "github.com/lib/pq"
 )
 
 var flyerDB *sql.DB
 
 func findDBCollision(inputHash string, startIndex, searchLength int) string {
 	// Connect to database
-	dbConfig, er := flyerdbutil.ReadConfig(dbConfigFilePath)
-	if er != nil {
-		log.Fatal("Failed to get Database Config data - ", er.Error())
-	}
-	flyerDB, er = flyerdbutil.Connect("server", dbConfig)
+	/*
+		dbConfig, er := flyerdbutil.ReadConfig(dbConfigFilePath)
+		if er != nil {
+			log.Fatal("Failed to get Database Config data - ", er.Error())
+		}
+	*/
+	var er error
+	flyerDB, er = flyerdbutil.ConnectHard()
 	if er != nil {
 		log.Fatal("Database connection failed - ", er.Error())
 	}
@@ -31,9 +31,8 @@ func findDBCollision(inputHash string, startIndex, searchLength int) string {
 		if er != nil {
 			return "" // get ready to redo this
 		}
-		compareHash := fmt.Sprintf("%d", cityhashutil.GetStrCode64Hash(word))
 
-		if compareHash == inputHash {
+		if compare(inputHash, word) {
 			return word
 		}
 	}
