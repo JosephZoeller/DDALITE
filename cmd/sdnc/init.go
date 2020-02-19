1cd6b4c51e562de6c6c63b00a76a15677c5843ec
ken343 <kmcdole21@gmail.com>
2020-02-18 18:53:35 -0600

    Created Temporary Dictionary Length
    Will need Jaeik to show me how to get the number of records from
    database-dictionary. This function will be in init().

--- cmd/sdnc/init.go
+++ cmd/sdnc/init.go
@@ -5,7 +5,10 @@ import (
 	"os"
 )
 
-var workerAddrs []string
+var (
+	dictionaryLength string = 10 //!! WARNING THIS IS ONLY TEMPORARY PLEASE ADD INIT LOGIC TO GET ACTUAL LENGTH FROM DB
+	workerAddrs      []string
+)
 
 // What is this for?
 
