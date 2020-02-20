7084f7ca336b79f7b23bf248811527c5afd5d548
Jaeik Oh <flyerjayden@gmail.com>
2020-02-20 01:52:32 -0600

    Deploying collider 0.4

--- cmd/collider/response.go
+++ cmd/collider/response.go
@@ -6,12 +6,11 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"github.com/200106-uta-go/JKJP2/pkg/cityhashutil"
 
+	"github.com/200106-uta-go/JKJP2/pkg/cityhashutil"
 )
 
 // Data is response data struct
-
 
 // Resp responses to request in JSON format
 func Resp(w http.ResponseWriter, r *http.Request) {
@@ -19,18 +18,18 @@ func Resp(w http.ResponseWriter, r *http.Request) {
 	// Parse request values
 	hash := r.PostFormValue("hash")
 	index, er := strconv.Atoi(r.PostFormValue("index"))
-	if (er != nil) {
+	if er != nil {
 		fmt.Fprint(w, "")
 		return
 	}
 	length, er := strconv.Atoi(r.PostFormValue("length"))
-	if (er != nil) {
+	if er != nil {
 		fmt.Fprint(w, "")
 		return
 	}
 
 	// Retrieve collision
-	response := cityhashutil.HashCollision {
+	response := cityhashutil.HashCollision{
 		InputHash: hash,
 		Collision: findCollisionFile(hash, index, length),
 	}
