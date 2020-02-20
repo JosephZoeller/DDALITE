d025d64fa3251eee6bc7d5cbf7981223378f8967
Jaeik Oh <flyerjayden@gmail.com>
2020-02-20 06:14:31 -0600

    HTML debug 8

--- cmd/sdnc/loading.go
+++ cmd/sdnc/loading.go
@@ -11,5 +11,5 @@ func loading(rw http.ResponseWriter, req *http.Request) {
 	hash := req.FormValue("hash")
 	instances := req.FormValue("instances")
 	go http.FileServer(http.Dir("html/loading.html"))
-	go http.Redirect(rw, req, "/result/?hash="+hash+"&instances="+instances, 418)
+	http.Redirect(rw, req, "/result/?hash="+hash+"&instances="+instances, 418)
 }
