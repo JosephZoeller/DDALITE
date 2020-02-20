992cd812c31df7622f5b004545baec5c40149f9e
Jaeik Oh <flyerjayden@gmail.com>
2020-02-20 07:43:45 -0600

    Adding HTML result page debug 1

--- cmd/sdnc/main.go
+++ cmd/sdnc/main.go
@@ -17,7 +17,7 @@ func main() {
 
 	fmt.Println("SDN Controller now listening on port 8080")
 	http.Handle("/", http.FileServer(http.Dir("cmd/sdnc/html")))
-	http.HandleFunc("/loading", processing)
+	http.HandleFunc("/processing", processing)
 	http.HandleFunc("/result", listenForClient)
 	http.ListenAndServe(":8080", nil)
 	signalChan := make(chan os.Signal, 1)
